fd70618205f160bbaff4f1d0615fca0a13727318
Artur Janik <sidsky@o2.pl>
2019-08-05 22:03:36 +0200

    Added router init

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 import './fonts.css';
 import './index.css';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import history from './utilities/history';
+
+const app = (
+  <Router history={history}>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 serviceWorker.unregister();
