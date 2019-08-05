a474ae80741f3a94c55851321a6e264b7e630a4c
Artur Janik <sidsky@o2.pl>
2019-08-05 22:28:06 +0200

    Added basic routes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import {
 } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
+import Map from './containers/Map/Map';
+import Document from './containers/Document/Document';
+
+const routes = (
+  <Switch>
+    <Route exact path="/polityka-prywatnosci" render={() => <Document show="privacy" />} />
+    <Route exact path="/o-projekcie" render={() => <Document show="about" />} />
+    <Route component={Map} />
+  </Switch>
+)
 
 function App() {
   return (
     <Layout>
-      <div>App.js</div>
+      {routes}
     </Layout>
   );
 }
