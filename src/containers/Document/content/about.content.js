f32795f3be60ed13d2d8a0fde8cd92fcbdf75d3b
Artur Janik <sidsky@o2.pl>
2019-08-08 12:42:09 +0200

    Updated About doc

--- src/containers/Document/content/about.content.js
+++ src/containers/Document/content/about.content.js
@@ -12,17 +12,24 @@ const content = (
     <p>Nie ukrywam, że dalszy rozwój mapy jest silnie uzależniony od aktywności użytkowników.</p>
     <h3>Założenia</h3>
     <ul>
-      <li>mapa obejmuje wyłącznie granice miasta Toruń</li>
-      <li>drogi są prezentowane w formie fragmentów, których podział opiera się zazwyczaj o rodzaj nawierzchni lub najważniejsze w mojej opinii ulice przecinające daną DDR</li>
-      <li>jezdnie z kontraruchem zostają pominięte chyba, że stanowią spójne przedłużenie drogi dla rowerów</li>
-      <li>Starówka zostaje pominięta</li>
-      <li>z czasem mapa może być uzupełniona o nowe warstwy - w najbliższych planach jest mapa stacji roweru miejskiego oraz mapa serwisów i sklepów rowerowych</li>
+      <li>Mapa obejmuje wyłącznie granice miasta Toruń.</li>
+      <li>Drogi są prezentowane w formie fragmentów, których podział opiera się zazwyczaj o rodzaj nawierzchni lub najważniejsze w mojej opinii ulice przecinające daną DDR.</li>
+      <li>Jezdnie z kontraruchem zostają pominięte chyba, że stanowią spójne przedłużenie drogi dla rowerów.</li>
+      <li>Starówka zostaje pominięta.</li>
+      <li>Z czasem mapa może być uzupełniona o nowe warstwy - w najbliższych planach jest mapa serwisów i sklepów rowerowych.</li>
+    </ul>
+    <h3>Aktualizacja 08.08.2019</h3>
+    <ul>
+      <li>Warstwa z danymi o ostrzeżeniach została ograniczona do robót budowlanych. Uznałem, że ze wszystkich trzech typów prezentowanych tam wcześniej informacji to właśnie roboty budowlane mają największą wartość dla użytkownika.</li>
+      <li>Została dodana warstwa przedstawiająca rozmieszczenie stacji Torvelo. Ze względów techniczych nie przewiduję na razie rozszerzenia informacji o poszczególnych stacjach.</li>
+      <li>Mapa powinna teraz działać poprawnie na przeglądarce IE11.</li>
+      <li>Poprawiłem drobne błędy w stopce i przypisach mapy - dziękuję za zwrócenie uwagi jednemu z użytkowników!</li>
     </ul>
     <hr />
     <h2>FAQ</h2>
     <h3>Jak mogę pomóc w tworzeniu mapy?</h3>
     <p>
-      Możesz zgłosić uwagi za pomocą anonimowego formularza kontaktowego. Będę wdzięczny za wszelkie informacje na temat braków i nieścisłości w mojej mapie, robót drogowych obejmujących DDR itp. Nie mam żadnych związków z władzami miasta i robię mapę pro bono więc jakieś wpadki mogą się zawsze zdarzyć.
+      Możesz zgłosić uwagi za pomocą anonimowego formularza kontaktowego. Będę wdzięczny za wszelkie informacje na temat braków i nieścisłości w mojej mapie, robót drogowych obejmujących DDR itp. Nie mam żadnych związków z władzami miasta i robię mapę pro publico bono więc jakieś wpadki mogą się zawsze zdarzyć.
     </p>
     <h3>Czy po wszystkich zaznaczonych drogach można legalnie poruszać się rowerem?</h3>
     <p>
@@ -45,6 +52,7 @@ const content = (
     <hr />
     <h2>Uwagi końcowe</h2>
     <p>Nośta kaski i złaźta z rowerów na przejściach. Czaszka jest jedna, a dwie tony blachy nie wybaczają ;)</p>
+    <p>Pozdrawiam, Artur</p>
   </React.Fragment>
 );
 
