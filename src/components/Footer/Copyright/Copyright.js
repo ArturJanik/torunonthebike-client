8768b28a0ce409ea08c23abf81d1e3be55c210a7
arturjanik <sidsky@o2.pl>
2022-08-11 22:46:08 +0200

    Quickfix + footer date

--- src/components/Footer/Copyright/Copyright.js
+++ src/components/Footer/Copyright/Copyright.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Copyright.module.css';
 
-const Copyright = () => <div className={ styles.copyright }>© torun.onthe.bike 2019</div>;
+const Copyright = () => <div className={ styles.copyright }>© torun.onthe.bike 2019-2022</div>;
 
 export default Copyright;
